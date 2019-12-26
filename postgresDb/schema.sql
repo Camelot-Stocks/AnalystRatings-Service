1847a1e2a772eb766565528048cac0cfc2efaba0
Hien Phuong <hien@MacBook-Pro-3.local>
2019-12-26 15:04:29 -0800

    Seed each table one by one

--- postgresDb/schema.sql
+++ postgresDb/schema.sql
@@ -1,21 +1,21 @@
 
 DROP TABLE IF EXISTS stocks, analysts, reviews;
  CREATE TABLE stocks (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name TEXT,
   buySummary VARCHAR(150),
   sellSummary VARCHAR(150)
  );
 
 CREATE TABLE analysts (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name TEXT,
   year SMALLINT,
   company TEXT
 );
 
 CREATE TABLE reviews (
-  id SERIAL PRIMARY KEY, 
+  id INTEGER PRIMARY KEY, 
   stockId INTEGER,
   analystId INTEGER,
   buy SMALLINT,
--- postgresDb/seeder.js
+++ postgresDb/seeder.js
@@ -1,5 +1,6 @@
 const {Pool} = require('pg');
-const {queryStr, queryAnalysts, reviewsQuery} = require('./dataGenerator.js');
+// const {queryStr, queryAnalysts, reviewsQuery} = require('./dataGenerator.js');
+const faker = require('faker');
 // const connectionString = 'postgressql://hien@localhost:5432/stocks_db'; 
 
 const pool = new Pool({
@@ -8,6 +9,53 @@ const pool = new Pool({
   port: 5432,
   database: 'stocks_db'
 });
+
+// data for stocks table
+let queryStr = 'INSERT INTO stocks (id, name, buysummary, sellsummary) VALUES';
+for (var i = 1; i < 1000001; i++) {
+  let id = i;
+  let name = faker.company.companyName();
+  if (name.includes("'")) {
+    name = name.replace(/'/g, '\'\'');
+  };
+  let buySummary = faker.lorem.sentence();
+  let sellSummary = faker.lorem.sentence();
+  queryStr+= `(${id},'${name}', '${buySummary}', '${sellSummary}'),`
+};
+queryStr = queryStr.slice(0, -1) + ';';
+
+//data for analysts table
+let range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+let years = range(2019, 1995, -1);
+let queryAnalysts = 'INSERT INTO analysts (id, name, year, company) VALUES';
+  for (var i = 1; i < 1000001; i++) {
+  let id = i;
+    let name = faker.name.firstName() + ' ' + faker.name.lastName();
+    if (name.includes("'")) {
+      name = name.replace(/'/g, '\'\'');
+    };
+    let year = years[Math.floor(Math.random()*years.length)];
+    let company = faker.company.companyName();
+    if (company.includes("'")) {
+      company = company.replace(/'/g, '\'\'');
+    };
+    queryAnalysts+= `(${id}, '${name}', '${year}', '${company}'),`
+  };
+  queryAnalysts = queryAnalysts.slice(0, -1) + ';';
+
+
+  //data for reviews table
+  let reviewsQuery = 'INSERT INTO reviews (id, stockId, analystId, buy, hold, sell) VALUES';
+  for (var i = 1; i < 100000001; i++) {
+    let id = i;
+    let stockId = Math.floor(Math.random() * Math.floor(1000000)) + 1;
+    let analystId = Math.floor(Math.random() * Math.floor(1000000)) + 1;
+    let buy = Math.floor(Math.random() * 2);
+    let hold = Math.floor(Math.random() * 2);
+    let sell = Math.floor(Math.random() * 2);
+    reviewsQuery += `(${id}, '${stockId}', '${analystId}', '${buy}', '${hold}', '${sell}'),`;
+  };
+  reviewsQuery = reviewsQuery.slice(0, -1) + ';';
 
 let seedStocks = () => {
   pool.query(queryStr, (err, res) => {
@@ -37,9 +85,9 @@ pool.connect()
   .then(() => console.log('connected successfully'))
   .then(() => seedStocks())
   .then(() => console.log('Seed stocks successful!'))
-  // .then(() => seedAnalysts())
-  // .then(() => console.log('Seed analysts successfully!'))
-  // .then(() => seedReviews())
-  // .then(() => console.log('Seed reviews successfully!'))
+  .then(() => seedAnalysts())
+  .then(() => console.log('Seed analysts successfully!'))
+  .then(() => seedReviews())
+  .then(() => console.log('Seed reviews successfully!'))
   .catch(e => console.log(e))
   .then(() => pool.end())
