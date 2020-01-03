7014244d080a08dd864daac094a7cb5b5fe9db43
Hien Phuong <hien@MacBook-Pro-3.local>
2020-01-03 11:25:33 -0800

    Seed files for cassandra

--- cassandradb/seed/analysts.js
+++ cassandradb/seed/analysts.js
@@ -4,29 +4,42 @@ var writer = csvWriter();
 const faker = require('faker');
 var counter = 0;
 
-let range = (start, stop, step) =>Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+let range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 let years = range(2019, 1995, -1);
 const analystsGen = () => {
   writer.pipe(fs.createWriteStream('analysts.csv'));
-  for (var i = 1; i < 100; i++) {
+  for (var i = 1; i < 1000001; i++) {
+    if (i === 1) {
+      console.log('start seeding')
+    }
     let name = faker.name.firstName() + ' ' + faker.name.lastName();
     if (name.includes("'")) {
       name = name.replace(/'/g, '\'\'');
     };
     let year = years[Math.floor(Math.random()*years.length)];
     let company = faker.company.companyName();
-      if (company.includes("'")) {
-        company = company.replace(/'/g, '\'\'');
-      };
+    if (company.includes("'")) {
+      company = company.replace(/'/g, '\'\'');
+    };
+    if (company.includes('and')) {
+      company = company.replace(/,/g, '')
+      company = company.replace(/,/g, '')
+    }
+    if (i === 500000) {
+      console.log('halfway there');
+    }
+    if (i === 1000001) {
+      console.log('Creating CSV done')
+    }
     writer.write({
-      id: counter++,
+      id: i,
       name: name,
       year: year,
       company: company
     })
   }
   writer.end();
-  console.log('generating done');
+  console.log('generating analysts done');
 }
 
 analystsGen();
