48c65b9c3fed34b6cd3e1965e9cb4b43be0b58b3
Hien Phuong <hien@MacBook-Pro-3.local>
2020-01-03 11:25:33 -0800

    Seed files for cassandra

--- cassandradb/seed/stocks.js
+++ cassandradb/seed/stocks.js
@@ -2,26 +2,29 @@ const csvWriter = require('csv-write-stream');
 const fs = require('fs');
 var writer = csvWriter();
 const faker = require('faker');
-var counter = 0;
 
 const stocksGen = () => {
   writer.pipe(fs.createWriteStream('stocks.csv'));
-  for (var i = 1; i < 100; i++) {
+  for (var i = 1; i < 1000001; i++) {
     let name = faker.company.companyName();
     if (name.includes("'")) {
       name = name.replace(/'/g, '\'\'');
     };
+    if (name.includes('and')) {
+      name = name.replace(/,/g, '')
+      name = name.replace(/,/g, '')
+    }
     let buySummary = faker.lorem.sentence();
     let sellSummary = faker.lorem.sentence();
     writer.write({
-      id: counter++,
+      id: i,
       name: name,
       buySummary: buySummary,
       sellSummary: sellSummary
     })
   }
   writer.end();
-  console.log('generating done');
+  console.log('generating stocks done');
 }
 
 stocksGen();
