2c15bf6c91f61525c099143ee7ec748ca4f37135
Hien Phuong <hien@MacBook-Pro-3.local>
2020-01-08 19:39:53 -0800

    Inplement load bouncer to stress test

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.get('/ratings/getData/:id', (req, res) => {
     var id = req.params.id;
     controller.Get.getReviews(id, res);
